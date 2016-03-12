25bc73a1a27fd511e2a3ac7e152b8eda01f9125e
Daniel Konovalenko <ddkono@gmail.com>
2016-03-12 22:48:57 +0300

    Changed pip version in vagrant_conf/install.sh to 7.1.2 in order to keep compatibility with python 3.2

--- vagrant_conf/install.sh
+++ vagrant_conf/install.sh
@@ -3,7 +3,7 @@
 sudo apt-get update -y
 sudo apt-get install vim python-virtualenv python3-dev python3-setuptools -y
 
-sudo easy_install3 pip
+sudo easy_install3 "pip==7.1.2"
 
 cp /vagrant/example.db /vagrant/pythontutor.db
 
